a0665160aa8a0d5fcd9fd3ea92cacd9ca069ab1f
CamillePar <camille.parillaud@gmail.com>
2015-12-13 16:42:52 +0100

    Revert "Version avant lien"

--- app/src/main/java/com/example/camille/tutorielconnexionphp/MainActivity.java
+++ app/src/main/java/com/example/camille/tutorielconnexionphp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.camille.tutorielconnexionphp;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,27 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameField = (EditText)findViewById(R.id.editText1);
-        passwordField = (EditText)findViewById(R.id.editText2);
-
-        status = (TextView)findViewById(R.id.textView6);
-        role = (TextView)findViewById(R.id.textView7);
-        method = (TextView)findViewById(R.id.textView9);
-    }
-
-    public void login(View view){
-        String username = usernameField.getText().toString();
-        String password = passwordField.getText().toString();
-        method.setText("Get Method");
-        new SigninActivity(this,status,role,0).execute(username,password);
-
-    }
-
-    public void loginPost(View view){
-        String username = usernameField.getText().toString();
-        String password = passwordField.getText().toString();
-        method.setText("Post Method");
-        new SigninActivity(this,status,role,1).execute(username,password);
     }
 
     @Override
